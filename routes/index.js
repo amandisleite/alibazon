e33610469903daa57acee434daea682d242c9825
Amanda Leite <mandirleite@gmail.com>
2022-02-04 22:30:20 -0300

    mens page data mostly set

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,23 @@ router.get('/', (req, res, next) => {
 const api = "https://osf-digital-backend-academy.herokuapp.com/api/"
 const secretKey = "$2a$08$QOu/RAl4bzs8fGWCYpiN5eYOMfPd2Nwmp4zlL3GPVp/RRkn3Mlgny"
 
-async function getapi(url) {
+async function getApi(url) {
     const response = await axios(url);
-    // const data = await response.express.json();
-    console.log(response)
     return response;
 }
 
 router.get('/categories/mens', async (req, res, next) => {
   try {
-    const mainCategory = await getapi(`${api}categories/mens?secretKey=${secretKey}`);
-    const subCategory = await getapi(`${api}categories/parent/mens?secretKey=${secretKey}`);
-    console.log(mainCategory)
-    console.log(subCategory)
+    const main = await getApi(`${api}categories/mens?secretKey=${secretKey}`);
+    const subClothing = await getApi(`${api}categories/parent/mens-clothing?secretKey=${secretKey}`);
+    const subAccessories = await getApi(`${api}categories/parent/mens-accessories?secretKey=${secretKey}`);
+    
     res.render('index', {
-      mainName: mainCategory.data.name,
-      mainDescription: mainCategory.data.page_description,
-      subData: subCategory.data,
-      subName: subCategory.data.name,
-      subDescription: subCategory.data.page_description,
-      subImage: subCategory.data.image
-     })
+      mainName: main.data.name,
+      mainDescription: main.data.page_description,
+      subDataClothing: subClothing.data,
+      subDataAccessories: subAccessories.data
+    })
 
   } catch (err) { next(err) }
 
