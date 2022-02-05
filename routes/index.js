155b670fa2c0ba6c9773cea98b3e9b9530cfcbdc
Amanda Leite <mandirleite@gmail.com>
2022-02-04 21:43:47 -0300

    test with routes returning data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Alibazon' });
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
+
+const api = "https://osf-digital-backend-academy.herokuapp.com/api/"
+const secretKey = "$2a$08$QOu/RAl4bzs8fGWCYpiN5eYOMfPd2Nwmp4zlL3GPVp/RRkn3Mlgny"
+
+async function getapi(url) {
+    const response = await axios(url);
+    // const data = await response.express.json();
+    console.log(response)
+    return response;
+}
+
+router.get('/categories/mens', async (req, res, next) => {
+  try {
+    const mainCategory = await getapi(`${api}categories/mens?secretKey=${secretKey}`);
+    const subCategory = await getapi(`${api}categories/parent/mens?secretKey=${secretKey}`);
+    console.log(mainCategory)
+    console.log(subCategory)
+    res.render('index', {
+      mainName: mainCategory.data.name,
+      mainDescription: mainCategory.data.page_description,
+      subData: subCategory.data,
+      subName: subCategory.data.name,
+      subDescription: subCategory.data.page_description,
+      subImage: subCategory.data.image
+     })
+
+  } catch (err) { next(err) }
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 
-app.listen(3010, () => { console.log('servidor rodando na porta 3010 ')})
+app.listen(3010, () => { console.log('servidor rodando na porta 3010')})
