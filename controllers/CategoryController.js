aaab2dd34a046f1ab8c8faf5d460e0097bfb2e55
Amanda Leite <mandirleite@gmail.com>
2022-02-07 11:46:38 -0300

    refactoring of controller getAllSubCategories method

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -7,26 +7,24 @@ class CategoryController {
     
     static async getAllSubCategories(req, res, next) {
         const category = req.params.category;
-        console.log(category)
-
-        const subCategories = ['clothing', 'accessories', 'jewelry']
+        const subCategories = ['clothing', 'accessories', 'jewelry'];
 
         try {
           const main = await CategoriesServices.getData(`${api}categories/${category}?secretKey=${secretKey}`);
-          const subClothing = await CategoriesServices.getData(`${api}categories/parent/${category}-clothing?secretKey=${secretKey}`);
-          const subAccessories = await CategoriesServices.getData(`${api}categories/parent/${category}-accessories?secretKey=${secretKey}`);
-          const subJewelry = await CategoriesServices.getData(`${api}categories/parent/${category}-jewelry?secretKey=${secretKey}`);
-          
-          res.render(`${category}`, {
-            mainName: main.data.name,
-            mainDescription: main.data.page_description,
-            subDataClothing: subClothing.data,
-            subDataAccessories: subAccessories.data,
-            subDataJewlery: subJewelry.data
-          })
-      
-        } catch (err) { next(err) }
-    }
-}
+          let subResults = [];
 
-module.exports = CategoryController;
+          for (let subCat of subCategories) {
+            const sub = await CategoriesServices.getData(`${api}categories/parent/${category}-${subCat}?secretKey=${secretKey}`)
+            subResults.push(sub.data)
+          }
+
+          res.render(`${category}`, {
+            mainData: main.data,
+            subResults
+          })
+        
+        } catch (err) { next(err) }
+      }
+    }
+    
+    module.exports = CategoryController;
--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,4 @@ router.get('/', (req, res, next) => {
 
 router.get('/:category', CategoryController.getAllSubCategories);
 
-// router.get('/womens', async (req, res, next) => {
-//   try {
-//     const main = await getApi(`${api}categories/womens?secretKey=${secretKey}`);
-//     const subClothing = await getApi(`${api}categories/parent/womens-clothing?secretKey=${secretKey}`);
-//     const subJewlery = await getApi(`${api}categories/parent/womens-jewlery?secretKey=${secretKey}`);
-//     const subAccessories = await getApi(`${api}categories/parent/womens-accessories?secretKey=${secretKey}`);
-
-    
-//     res.render('womens', {
-//       mainName: main.data.name,
-//       mainDescription: main.data.page_description,
-//       subDataClothing: subClothing.data,
-//       subDataAccessories: subAccessories.data,
-//       subDataJewlery: subJewlery.data
-//     })
-
-//   } catch (err) { next(err) }
-
-// })
-
 module.exports = router;
