b6e2d46accb428a09762ea7f545d2a2ff5788164
Amanda Leite <mandirleite@gmail.com>
2022-02-23 13:32:17 -0300

    cart routes test working

---
+++ __tests__/apiRoutes-Cart.test.js
@@ -0,0 +1,57 @@
+const app = require('../app');
+const { CartServices, UserServices } = require('../services');
+
+describe('Cart', () => {
+
+    it('should add item to Cart', async () => {
+        const user = UserServices.loginUser('teste@gmail.com', '123456')
+        const resUser = await UserServices.signIn(user)
+        const token = resUser.data.token
+
+        const cart = CartServices.cartItem('25592211', '701643843459', '3')
+        
+        const res = await CartServices.sendDataCart(cart, token)
+        expect(res).toBeDefined()
+        expect(res.data.items[0].productId).toBe('25592211')
+        expect(res.config.method).toBe('post')
+
+    })
+
+    it('should return user Cart (that already exists) request', async () => {
+        const user = UserServices.loginUser('teste@gmail.com', '123456')
+        const resUser = await UserServices.signIn(user)
+        const token = resUser.data.token
+        
+        const res = await CartServices.getDataCart(token)
+        expect(res.status).toBe(200)
+        expect(res.data.items.length).toBeGreaterThanOrEqual(1)
+    })
+
+    it('should change quantity of item', async () => {
+        const user = UserServices.loginUser('teste@gmail.com', '123456')
+        const resUser = await UserServices.signIn(user)
+        const token = resUser.data.token
+
+        const cart = CartServices.cartItem('25592211', '701643843459', '5')
+        
+        const res = await CartServices.changeQuantityCart(cart, token)
+        expect(res).toBeDefined()
+        expect(res.data.items[0].quantity).toBe(5)
+        expect(res.config.method).toBe('post')
+
+    })
+
+    it('should delete item', async () => {
+        const user = UserServices.loginUser('teste@gmail.com', '123456')
+        const resUser = await UserServices.signIn(user)
+        const token = resUser.data.token
+
+        const cart = CartServices.deleteCartItem('25592211', '701643843459')
+        
+        const res = await CartServices.deleteDataCart(cart, token)
+        expect(res).toBeDefined()
+        expect(res.data).toBe('')
+        expect(res.config.method).toBe('delete')
+    })
+
+})
