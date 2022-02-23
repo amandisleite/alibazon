887d761162119a421c5581180fc58fb7348962ac
Amanda Leite <mandirleite@gmail.com>
2022-02-23 20:58:27 -0300

    refactoring of orders controller and services

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,59 +1,18 @@
 const { OrderNotAvailable } = require('../errors');
-const { OrderServices, CartServices, CategoriesServices } = require('../services');
+const { OrderServices, CartServices } = require('../services');
 
 class OrderController {
     
     static async getOrder(req, res, next) {
-        let eachProductId = 0;
-        let quantityProduct = 0;
-        let paymentId = 0;
-        let address = 0;
-        const productsPrices = [];
-        let productObj = {};
-        let priceProduct = 0;
-        const allOrders = [];
-        const everyAddress = [];
-        const everyPaymentId = [];
-
         try {
             const orders = await OrderServices.getDataOrder(req.cookies.token)
-            const orderData = orders.data
+            const orderData = OrderServices.returnItemsFromRequest(orders)
             
             if (orderData) {
-                for (let orderItems of orderData) {
-                    const orderItem = orderItems.items
-                    paymentId = orderItems.paymentId
-                    address = orderItems.address
-                    const perOrder = [];
-                    const pricePerOrder = [];
-                    const addressPerOrder = [];
-                    const paymentIdPerOrder = [];
-                    for (let eachProduct of orderItem) {
-                        eachProductId = eachProduct.productId
-                        priceProduct = eachProduct.variant.price
-                        quantityProduct = eachProduct.quantity
-                        priceProduct = priceProduct * quantityProduct
-                        pricePerOrder.push(priceProduct)
-                        addressPerOrder.push(address)
-                        paymentIdPerOrder.push(paymentId)
-
-                        const product = await CategoriesServices.getDataOneProduct(eachProductId);
-                        productObj = {
-                            productName: product.data[0].name,
-                            productId: product.data[0].id,
-                            price: priceProduct,
-                            quantity: quantityProduct,
-                            address: address, 
-                            paymentId: paymentId
-                        }
-
-                        perOrder.push(productObj)
-                    }
-                everyPaymentId.push(paymentIdPerOrder)
-                everyAddress.push(addressPerOrder)
-                productsPrices.push(pricePerOrder)
-                allOrders.push(perOrder)
-                }
+                const everyPaymentId = OrderServices.getOrderPaymentId(orderData)
+                const everyAddress = OrderServices.getOrderAddress(orderData)
+                const productsPrices = OrderServices.getOrderPrice(orderData)
+                const allOrders = await OrderServices.getOrderDetail(orderData)
 
                 const allPaymentsIds = OrderServices.uniqueValuesAtArray(everyPaymentId)
                 const allAddresses = OrderServices.uniqueValuesAtArray(everyAddress)
@@ -79,13 +38,10 @@ class OrderController {
 
         try {
             const cart = await CartServices.getDataCart(req.cookies.token);
-            const cartProducts = cart.data.items
-
+            const cartProducts = CartServices.returnItemsFromRequest(cart);
             const olderOrders = await OrderServices.getDataOrder(req.cookies.token)
             const paymentId = OrderServices.createPaymentId(olderOrders)
-
             const newOrder = OrderServices.createOrder(address, paymentId, cartProducts)
-
             const order = await OrderServices.sendDataOrder(newOrder, req.cookies.token)
             const orderData = order.data
 
--- services/OrderServices.js
+++ services/OrderServices.js
@@ -1,4 +1,5 @@
 const Services = require('./Services');
+const CategoriesServices = require('./CategoriesServices')
 
 const api = process.env.API_URL;
 const secretKey = process.env.SECRET_KEY;
@@ -53,6 +54,86 @@ class OrderServices extends Services {
         return paymentId;
     }
 
+    static returnItemsFromRequest(request) {
+        const allOrders = request.data
+        return allOrders;
+    }
+
+    static getOrderPaymentId(orders) {
+        const everyPaymentId = [];
+        for (let orderItems of orders) {
+            const orderItem = orderItems.items
+            const paymentId = orderItems.paymentId
+            const paymentIdPerOrder = [];
+            for (let eachProduct of orderItem) {
+                paymentIdPerOrder.push(paymentId)
+            }
+            everyPaymentId.push(paymentIdPerOrder)
+        }
+        return everyPaymentId;
+    }
+
+    static getOrderAddress(orders) {
+        const everyAddress = [];
+        for (let orderItems of orders) {
+            const orderItem = orderItems.items
+            const address = orderItems.address
+            const addressPerOrder = [];
+            for (let eachProduct of orderItem) {
+                addressPerOrder.push(address)
+            }
+            everyAddress.push(addressPerOrder)
+        }
+        return everyAddress;
+    }
+
+    static getOrderPrice(orders) {
+        const productsPrices = [];
+        for (let orderItems of orders) {
+            const orderItem = orderItems.items
+            const pricePerOrder = [];
+            for (let eachProduct of orderItem) {
+                let priceProduct = eachProduct.variant.price
+                const quantityProduct = eachProduct.quantity
+                priceProduct = priceProduct * quantityProduct
+                pricePerOrder.push(priceProduct)
+            }
+            productsPrices.push(pricePerOrder)
+        }
+        return productsPrices;
+    }
+
+    static async getOrderDetail(orders) {
+        let productObj = 0;
+        const allOrders = [];
+        for (let orderItems of orders) {
+            const orderItem = orderItems.items
+            const paymentId = orderItems.paymentId
+            const address = orderItems.address
+            const perOrder = [];
+            for (let eachProduct of orderItem) {
+                const eachProductId = eachProduct.productId
+                let priceProduct = eachProduct.variant.price
+                const quantityProduct = eachProduct.quantity
+                priceProduct = priceProduct * quantityProduct
+
+                const product = await CategoriesServices.getDataOneProduct(eachProductId);
+                productObj = {
+                    productName: product.data[0].name,
+                    productId: product.data[0].id,
+                    price: priceProduct,
+                    quantity: quantityProduct,
+                    address: address, 
+                    paymentId: paymentId
+                }
+
+                perOrder.push(productObj)
+
+            }
+            allOrders.push(perOrder)
+        }
+        return allOrders;
+    }
 }
 
 module.exports = OrderServices;
