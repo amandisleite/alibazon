3d41a6b014cac450567c5673b59bf1bd05765c6b
Amanda Leite <mandirleite@gmail.com>
2022-02-24 17:51:07 -0300

    refactoring user sign up and sign in in user controller and service

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,20 +1,12 @@
 const { UserServices } = require('../services');
-const { UserAlreadyExists, CredentialsAreIncorrect } = require('../errors');
 
 class UserController {
   
     static async signUpUser(req, res, next) {
         const { username, useremail, userpassword } = req.body
-
-        const newUser = UserServices.createUser(username, useremail, userpassword);
-
         try {
-            const userExists = await UserServices.signUp(newUser)
-            if (userExists === 'Request failed with status code 400') {
-                throw new UserAlreadyExists(useremail);
-            } else {
-                await UserServices.signUp(newUser)    
-            }
+            await UserServices.signUp(username, useremail, userpassword)
+            res.redirect('/signin')
         } catch (err) {
             return next(err)
         }
@@ -22,13 +14,8 @@ class UserController {
 
     static async signInUser(req, res, next) {
         const { useremail, userpassword } = req.body
-        const user = UserServices.loginUser(useremail, userpassword);
-
         try {
-            const userLogged = await UserServices.signIn(user)
-            if (userLogged === 'Request failed with status code 400') {
-                throw new CredentialsAreIncorrect();
-            }
+            const userLogged = await UserServices.signIn(useremail, userpassword)
             res.cookie('token', userLogged.data.token)
             res.redirect('/')     
         } catch (err) {
--- services/UserServices.js
+++ services/UserServices.js
@@ -1,16 +1,29 @@
 const Services = require('./Services');
+const { UserAlreadyExists, CredentialsAreIncorrect } = require('../errors');
 
 const api = process.env.API_URL;
 const secretKey = process.env.SECRET_KEY;
 
 class UserServices extends Services {
 
-    static async signUp(user) {
-        return Services.sendData(`${api}auth/signup`, user)
+    static async signUp(name, email, password) {
+        const newUser = this.createUser(name, email, password);
+        const tryToSignUp = await Services.sendData(`${api}auth/signup`, newUser)
+        if (tryToSignUp === 'Request failed with status code 400') {
+            throw new UserAlreadyExists(email);
+        } else {
+            return tryToSignUp;
+        }
     }
 
-    static async signIn(user) {
-        return Services.sendData(`${api}auth/signin`, user)
+    static async signIn(email, password) {
+        const user = this.loginUser(email, password);
+        const userLogged = Services.sendData(`${api}auth/signin`, user)
+        if (userLogged === 'Request failed with status code 400') {
+            throw new CredentialsAreIncorrect();
+        } else {
+            return userLogged;
+        }
     }
 
     static createUser(name, email, password) {
